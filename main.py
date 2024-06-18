dd3506819b62727bcc7661841867db19186b7fff
Hélio do Nascimento <115810349+Puritoka@users.noreply.github.com>
2024-06-18 12:12:16 -0300

    Updata

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (QApplication, QMainWindow)
 from ui_main import Ui_MainWindow
+from cep_Api import postal
 import sys
 
 
@@ -12,6 +13,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle("CRM Santa Maria - Customer Relationship Management - Version 1.0.2.4")
         appIcon = QIcon(u"")  # Configuração do Favicon
         self.setWindowIcon(appIcon)
+
+        ###############################################################################################
+        # PREENCHER AUTOMATICAMENTE OS DADOS DO CEP
+        self.txt_cep_pesquisa.editingFinished.connect(self.consulta_cep)
+        ###############################################################################################
+
+    def consulta_cep(self):
+
+        campos = postal(self.txt_cep_pesquisa.text())
+
+        self.txt_logradouro.setText(campos[0])
+        self.txt_bairro.setText(campos[1])
+        self.txt_municipio.setText(campos[2])
+        self.txt_uf.setText(campos[3])
+        self.txt_cep.setText(campos[4])
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
